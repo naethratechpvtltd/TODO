95542da5d9d827b98d9edcb93277ff0903822608
Sathish <ramsatt@gmail.com>
2017-12-20 16:33:01 +0530

    First

---
+++ src/environments/firebase.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCipC1uJ9zY3pOF8fQ3woluk3qtmLBc-vU',
+  authDomain: 'angular-poc-bb48f.firebaseapp.com',
+  databaseURL: 'https://angular-poc-bb48f.firebaseio.com',
+  storageBucket: 'angular-poc-bb48f.appspot.com'
+};
