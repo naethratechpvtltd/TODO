88365126b59a445d3849ce7831e8134f45ff1c85
kumarasathisha <34370636+kumarasathisha@users.noreply.github.com>
2017-12-20 17:07:01 +0530

    Update app-header.component.ts

--- src/app/app-header.component.ts
+++ src/app/app-header.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 // New Comment Added
+// New Comment Added by Sathisha
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
